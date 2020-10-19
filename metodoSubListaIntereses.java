6a9571454a2d064e2fd91eac52208ea9393c64f3
Jonathan Mendoza <52763528+bl4sv@users.noreply.github.com>
2020-10-19 08:18:23 -0600

    Add files via upload

---
+++ metodoSubListaIntereses.java
@@ -0,0 +1,16 @@
+package estructuraspp;
+/**
+ *
+ * @author Jonathan
+ */
+public class metodoSubListaIntereses {
+    
+        subListaIntereses inicio;
+        
+        //inserción al inicio
+        
+        //Buscar interés        //Verifica si un interés existe o no
+        
+        //Eliminar interés      /// Elimina un interés de la sublista del usuario
+
+}
